5f613b8a0c170f8404cb354b729ae134eebb9f52
John Barry <JohnBar216@gmail.com>
2016-03-29 20:22:30 -0500

    Finished Hello World and A Plus B

---
+++ A Plus B/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+/**
+ * This Program will take take 2 inputs, 
+ * 	confirm they are integers -1000 to 1000
+ * 	and print the sum of the two values
+ *
+ * Dev: John Barry
+ */
+func main() {
+	
+	// Declare vars
+	var a int32
+	var b int32
+
+	// Ask and listen for user input
+	fmt.Print("Please enter two integers betwen -1000 and 1000: ")
+	_, err := fmt.Scan(&a, &b)
+
+	// Run basic error check, exit if error occured
+	if err != nil {
+		fmt.Print("Error occured: ", err)
+		os.Exit(0)
+	}
+
+	// Check to make sure values are between -1000 and 100
+	if checkRanges(a,b) == false {
+		fmt.Print("Both values must be between -1000 and 1000")
+	} else {
+		fmt.Print( a + b )
+	}
+}
+
+
+/**
+ * Checks if a and b are both between -1000 and 1000
+ * @return bool 
+ */
+func checkRanges(a, b int32) bool {
+	
+	if a >= -1000 && a <= 1000 && b >= -1000 && b <= 1000 {
+		return true;
+	} else {
+		return false;
+	}
+}
--- Hello World/main.go
+++ Hello World/main.go
@@ -1,1 +1,14 @@
-asdfasdfsd
+package main
+
+import (
+	"fmt"
+)
+
+/**
+ * Dev: John Barry
+ */
+func main() {
+
+	// Print Hello World to the command promt
+	fmt.Print("Hello World") 
+}
